281f3352ba01bf9f2b36abe644424da2c00e25b8
Potompridumau <vfvfhsitv@gmail.com>
2024-11-27 17:51:55 +0300

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудникков в компании</h1>
+            <h2>Общее количество сотрудникков</h2>
+            <h2>Премию получат: </h2>
+        </div>
+    );
+}
+
+export default AppInfo;
